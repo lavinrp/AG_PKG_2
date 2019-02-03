9e36be48e4ff762d286d25928c120817955129c7
Ryan Lavin <lavinrp@gmail.com>
2019-02-03 06:07:52 -0500

    now with small notifications of how things are going. And it works. If it didn't before this commit. Idk. Its late. Still no real error stuff. Only tested on linux. Single thread. :(

--- main.cpp
+++ main.cpp
@@ -23,6 +23,8 @@
 
 #include <boost/filesystem.hpp>
 
+#include <nlohmann/json.hpp>
+
 #include <fstream>
 #include <iostream>
 #include <memory>
@@ -80,6 +82,64 @@ void download_file(const std::string& uri_to_download, const std::string& instal
     }
 }
 
+void create_vsc_proj_in_folder(std::string project_dir, std::string install_dir)
+{
+
+    // ensure project dir exists
+    boost::filesystem::path vscode_dir(project_dir);
+    vscode_dir /= ".vscode";
+    boost::filesystem::create_directories(vscode_dir);
+
+    // settings path
+    boost::filesystem::path settings_filepath = vscode_dir / "settings.json";
+    
+    // dependency locations
+    boost::filesystem::path base_dependency_dir = boost::filesystem::path(install_dir) / "AG_Dependencies";
+    // boost
+    boost::filesystem::path boost_root = base_dependency_dir / "boost_1_69_0_libstdcpp_clang_7";
+    // sfml
+    boost::filesystem::path sfml_root = base_dependency_dir / "SFML-2-5-1_clang_7_0__libstdcpp";
+    boost::filesystem::path sfml_lib_dir = sfml_root / "lib";
+    boost::filesystem::path sfml_cmake_dir = sfml_lib_dir / "cmake" / "SFML";
+    // tgui
+    boost::filesystem::path tgui_root = base_dependency_dir / "TGUI-0.8.3_clang_7_libstdcpp";
+    boost::filesystem::path tgui_lib_dir = tgui_root / "lib";
+    boost::filesystem::path tgui_cmake_dir = tgui_lib_dir / "lib" / "cmake" / "TGUI";
+    // box
+    boost::filesystem::path box2d_root = base_dependency_dir / "Box2D-Jan-27-2019__clang__libstdcpp";
+
+    // create json for settings
+    auto vs_code_settings = R"(
+        {   
+            "cmake.configureSettings" : {
+                "BOOST_ROOT" : "AG_PKG_ERROR_PATH",
+                "SFML_DIR" : "AG_PKG_ERROR_PATH",
+                "TGUI_DIR" : "AG_PKG_ERROR_PATH",
+                "BOX2D_DIR" : "AG_PKG_ERROR_PATH"
+            },
+            "lldb.launch.env": {
+                "LD_LIBRARY_PATH":"AG_PKG_ERROR_PATH"
+            }
+        }
+    )"_json;
+    vs_code_settings["cmake.configureSettings"]["BOOST_ROOT"] = boost_root.string();
+    vs_code_settings["cmake.configureSettings"]["SFML_DIR"] = sfml_cmake_dir.string();
+    vs_code_settings["cmake.configureSettings"]["TGUI_DIR"] = tgui_cmake_dir.string();
+    vs_code_settings["cmake.configureSettings"]["BOX2D_DIR"] = box2d_root.string();
+    vs_code_settings["lldb.launch.env"]["LD_LIBRARY_PATH"] = sfml_lib_dir.string() + ":" + tgui_lib_dir.string();
+
+    // write to file
+    std::ofstream settings_out_file(settings_filepath.string(), std::ofstream::out | std::ofstream::app);
+    if(settings_out_file.good())
+    {
+        settings_out_file << vs_code_settings.dump(4);
+    }
+    else
+    {
+        std::cout << "Bad Settings file path" << std::endl;
+    }
+}
+
 int main()
 {
     // poco setup
@@ -92,13 +152,15 @@ int main()
 	Poco::Net::Context::Ptr ptrContext = new Poco::Net::Context(Poco::Net::Context::CLIENT_USE, "", "", "", Poco::Net::Context::VERIFY_STRICT, 9, true, "ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH");
 	Poco::Net::SSLManager::instance().initializeClient(0, ptrCert, ptrContext);
 
-
     //Define a form.
     nana::form fm {nullptr, {400, 300}, nana::form::appear::decorate<nana::form::appear::maximize>()};
     fm.caption("AG PKG");
     fm.events().unload([]{
         nana::API::exit_all();
     });
+
+    // status
+    nana::label status_label{fm, ""};
 
     // Install directory
     nana::label download_dir_lbl{fm, "Install Directory"};
@@ -118,7 +180,6 @@ int main()
             install_dir_textbox.append(folder.value().string(), false);
         }
     });
-
 
     // Project directory
     nana::label project_dir_lbl{fm, "Project Directory"};
@@ -143,6 +204,7 @@ int main()
     nana::button download_btn{fm, "Install"};
     download_btn.events().click([&]{
         std::cout << "downloading..." << std::endl;
+        status_label.caption("downloading...");
         boost::filesystem::path final_install_path(install_dir_textbox.caption());
         // ensure the path exists
         boost::filesystem::create_directories(final_install_path);
@@ -150,8 +212,17 @@ int main()
         boost::filesystem::path final_unzip_path = final_install_path / "AG_Dependencies_1_0_0";
         final_install_path /= "AG_Dependencies_1_0_0.zip";
         auto final_install_path_string = final_install_path.string();
+
+        // install
         download_file("https://www.dropbox.com/s/w18c6uoqxvm9gfh/Clang_7_stdlibcpp.zip?dl=1", final_install_path_string);
+
+        status_label.caption("unzipping...");
         unzip_file_to_dest(final_install_path_string, final_unzip_path.string());
+        
+        // project setup
+        status_label.caption("creating project...");
+        create_vsc_proj_in_folder(project_dir_textbox.caption(), final_unzip_path.string());
+        status_label.caption("done");
     });
 
     // layout form
@@ -172,7 +243,9 @@ int main()
             "<margin=[0, 5, 0, 0] search_for_project_directory_btn>"\
         ">"\
         "<>"\
+        "<>"\
         "<download_btn>"
+        "<status_label>"\
     );
 
     // add widgets to the form
@@ -183,6 +256,7 @@ int main()
     fm["download_btn"] << download_btn;
     fm["search_for_install_directory_btn"] << search_for_install_directory_btn;
     fm["search_for_project_directory_btn"] << search_for_project_directory_btn;
+    fm["status_label"] << status_label;
     fm.collocate();
 
     // show form
